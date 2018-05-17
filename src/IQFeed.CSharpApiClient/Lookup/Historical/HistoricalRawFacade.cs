32531335cf1d68f8a8ab63025e76f3eefa7c10c9
Mathieu Paquette <m.paquette@inovitex.com>
2018-05-17 09:27:21 -0400

    fix(lookup): added ConfigureAwait(false) inside all HistoricalRawFacade callers

--- src/IQFeed.CSharpApiClient/Lookup/Historical/HistoricalRawFacade.cs
+++ src/IQFeed.CSharpApiClient/Lookup/Historical/HistoricalRawFacade.cs
@@ -21,14 +21,14 @@ namespace IQFeed.CSharpApiClient.Lookup.Historical
             int? datapointsPerSend = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryTickDatapoints(symbol, maxDatapoints, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryTickDaysAsync(string symbol, int days, int? maxDatapoints = null, TimeSpan? beginFilterTime = null,
             TimeSpan? endFilterTime = null, int? dataDirection = null, string requestId = null, int? datapointsPerSend = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryTickDays(symbol, days, maxDatapoints, beginFilterTime, endFilterTime, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryTickTimeframeAsync(string symbol, DateTime? beginDate, DateTime? endDate, int? maxDatapoints = null,
@@ -39,14 +39,14 @@ namespace IQFeed.CSharpApiClient.Lookup.Historical
                 throw new ArgumentException("Begin date or End date must have value.");
 
             var request = _historicalRequestFormatter.ReqHistoryTickTimeframe(symbol, beginDate, endDate, maxDatapoints, beginFilterTime, endFilterTime, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryIntervalDatapointsAsync(string symbol, int interval, int maxDatapoints, int? dataDirection = null,
             string requestId = null, int? datapointsPerSend = null, HistoricalIntervalType? intervalType = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryIntervalDatapoints(symbol, interval, maxDatapoints, dataDirection, requestId, datapointsPerSend, intervalType);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryIntervalDaysAsync(string symbol, int interval, int days, int? maxDatapoints = null,
@@ -54,7 +54,7 @@ namespace IQFeed.CSharpApiClient.Lookup.Historical
             string requestId = null, int? datapointsPerSend = null, HistoricalIntervalType? intervalType = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryIntervalDays(symbol, interval, days, maxDatapoints, beginFilterTime, endFilterTime, dataDirection, requestId, datapointsPerSend, intervalType);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryIntervalTimeframeAsync(string symbol, int interval, DateTime? beginDate, DateTime? endDate,
@@ -64,14 +64,14 @@ namespace IQFeed.CSharpApiClient.Lookup.Historical
         {
             var request = _historicalRequestFormatter.ReqHistoryIntervalTimeframe(symbol, interval, beginDate, endDate,
                 maxDatapoints, beginFilterTime, endFilterTime, dataDirection, requestId, datapointsPerSend, intervalType);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryDailyDatapointsAsync(string symbol, int maxDatapoints, int? dataDirection = null,
             string requestId = null, int? datapointsPerSend = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryDailyDatapoints(symbol, maxDatapoints, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryDailyTimeframeAsync(string symbol, DateTime? beginDate, DateTime? endDate, int? maxDatapoints = null,
@@ -81,21 +81,21 @@ namespace IQFeed.CSharpApiClient.Lookup.Historical
                 throw new ArgumentException("Begin date or End date must have value.");
 
             var request = _historicalRequestFormatter.ReqHistoryDailyTimeframe(symbol, beginDate, endDate, maxDatapoints, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryWeeklyDatapointsAsync(string symbol, int maxDatapoints, int? dataDirection = null,
             string requestId = null, int? datapointsPerSend = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryWeeklyDatapoints(symbol, maxDatapoints, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
 
         public async Task<string> ReqHistoryMonthlyDatapointsAsync(string symbol, int maxDatapoints, int? dataDirection = null,
             string requestId = null, int? datapointsPerSend = null)
         {
             var request = _historicalRequestFormatter.ReqHistoryMonthlyDatapoints(symbol, maxDatapoints, dataDirection, requestId, datapointsPerSend);
-            return await _rawMessageHandler.GetFilenameAsync(request);
+            return await _rawMessageHandler.GetFilenameAsync(request).ConfigureAwait(false);
         }
     }
 }
