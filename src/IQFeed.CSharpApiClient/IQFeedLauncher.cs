392b80983fb73d90d9b906194557623b80fee7fa
Mathieu Paquette <m.paquette@inovitex.com>
2018-05-18 10:31:01 -0400

    feat(IQFeedLauncher): added busy waiting/retry until the admin port is available

--- src/IQFeed.CSharpApiClient/IQFeedLauncher.cs
+++ src/IQFeed.CSharpApiClient/IQFeedLauncher.cs
@@ -2,12 +2,13 @@
 using System.Configuration;
 using System.Threading;
 using IQFeed.CSharpApiClient.Extensions;
+using IQFeed.CSharpApiClient.Socket;
 
 namespace IQFeed.CSharpApiClient
 {
     public static class IQFeedLauncher
     {
-        public static void Start(string login = null, string password = null, string productId = null, string productVersion = null, int sleepTimeMs = 2500)
+        public static void Start(string login = null, string password = null, string productId = null, string productVersion = null, int connectionTimeoutMs = 100, int retry = 50)
         {
             var appSettings = ConfigurationManager.AppSettings;
 
@@ -35,7 +36,10 @@ namespace IQFeed.CSharpApiClient
                 "IQConnect.exe", $"-product {productId} -version {productVersion} -login {login} -password {password} -autoconnect"
             );
 
-            Thread.Sleep(sleepTimeMs); // TODO: replace this by an active polling to check if the feed has been started
+            var adminPort = SocketDiagnostic.IsPortOpen(IQFeedDefault.Hostname, IQFeedDefault.AdminPort, connectionTimeoutMs, retry);
+
+            if (!adminPort)
+                throw new Exception($"Can't establish TCP connection with host: {IQFeedDefault.Hostname}:{IQFeedDefault.AdminPort}");
         }
     }
 }
---
+++ src/IQFeed.CSharpApiClient/Socket/SocketDiagnostic.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+
+namespace IQFeed.CSharpApiClient.Socket
+{
+    public static class SocketDiagnostic
+    {
+        public static bool IsPortOpen(string host, int port, int timeout, int retry)
+        {
+            var retryCount = 0;
+            while (retryCount < retry)
+            {
+                if (retryCount > 0)
+                    Thread.Sleep(timeout);
+
+                try
+                {
+                    using (var client = new TcpClient())
+                    {
+                        var result = client.BeginConnect(host, port, null, null);
+                        var success = result.AsyncWaitHandle.WaitOne(timeout);
+                        if (success)
+                            return true;
+
+                        client.EndConnect(result);
+                    }
+                }
+                catch
+                {
+                    // ignored
+                }
+                finally { retryCount++; }
+            }
+
+            return false;
+        }
+    }
+}
