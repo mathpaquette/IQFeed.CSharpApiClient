bc1adc5621fbcb2215b437fe1410ebec4c4bfb54
Mathieu Paquette <m.paquette@inovitex.com>
2018-06-02 13:02:00 -0400

    feat(symbol): added ListedMarket

---
+++ src/IQFeed.CSharpApiClient/Lookup/Symbol/ListedMarket.cs
@@ -0,0 +1,310 @@
+﻿namespace IQFeed.CSharpApiClient.Lookup.Symbol
+{
+    public enum ListedMarket
+    {
+        /// <summary>
+        /// NASDAQ National market
+        /// </summary>
+        NMS = 1,
+
+        /// <summary>
+        /// Small Capital Market
+        /// </summary>
+        SMCAP = 2,
+
+        /// <summary>
+        /// NASDAQ other OTC
+        /// </summary>
+        OTC = 3,
+
+        /// <summary>
+        /// NASDAQ OTC Bulletin Board
+        /// </summary>
+        OTCBB = 4,
+
+        /// <summary>
+        /// NASDAQ
+        /// </summary>
+        NASDAQ = 5,
+
+        /// <summary>
+        /// NYSE American Exchange
+        /// </summary>
+        NYSE_AMEX = 6,
+
+        /// <summary>
+        /// New York Stock Exchange
+        /// </summary>
+        NYSE = 7,
+
+        /// <summary>
+        /// Chicago Stock Exchange
+        /// </summary>
+        CHX = 8,
+
+        /// <summary>
+        /// Philadelphia Stock Exchange
+        /// </summary>
+        PHLX = 9,
+
+        /// <summary>
+        /// National Stock Exchange
+        /// </summary>
+        NSX = 10,
+
+        /// <summary>
+        /// NYSE Archipelago
+        /// </summary>
+        ARCA = 11,
+
+        /// <summary>
+        /// Boston Stock Exchange
+        /// </summary>
+        BX = 12,
+
+        /// <summary>
+        /// Chicago Board Options Exchange
+        /// </summary>
+        CBOE = 13,
+
+        /// <summary>
+        /// OPRA System
+        /// </summary>
+        OPRA = 14,
+
+        /// <summary>
+        /// NASDAQ Alternate Display facility
+        /// </summary>
+        NASD_ADF = 15,
+
+        /// <summary>
+        /// International Stock Exchange
+        /// </summary>
+        ISE = 16,
+
+        /// <summary>
+        /// Boston Options Exchange
+        /// </summary>
+        BOX = 17,
+
+        /// <summary>
+        /// Better Alternative Trading System
+        /// </summary>
+        BATS = 18,
+
+        /// <summary>
+        /// Philadelphia Board of Trade
+        /// </summary>
+        PBOT = 20,
+
+        /// <summary>
+        /// NYSE_AMEX Options
+        /// </summary>
+        NYSE_AMEX_OPTIONS = 25,
+
+        /// <summary>
+        /// DTN(Calculated/Index/Statistic)
+        /// </summary>
+        DTN = 27,
+
+        /// <summary>
+        /// Chicago Board Of Trade
+        /// </summary>
+        CBOT = 30,
+
+        /// <summary>
+        /// Dow Jones (CBOT)
+        /// </summary>
+        DJ = 31,
+
+        /// <summary>
+        /// CBOE Futures Exchange
+        /// </summary>
+        CFE = 32,
+
+        /// <summary>
+        /// Kansas City Board Of Trade
+        /// </summary>
+        KCBOT = 33,
+
+        /// <summary>
+        /// Chicago Mercantile Exchange
+        /// </summary>
+        CME = 34,
+
+        /// <summary>
+        /// Minneapolis Grain Exchange
+        /// </summary>
+        MGE = 35,
+
+        /// <summary>
+        /// New York Mercantile Exchange
+        /// </summary>
+        NYMEX = 36,
+
+        /// <summary>
+        /// Commodities Exchange Center
+        /// </summary>
+        COMEX = 37,
+
+        /// <summary>
+        /// International Commodities Exchange Futures US
+        /// </summary>
+        ICEFU = 38,
+
+        /// <summary>
+        /// NYSE LIFFE Metals
+        /// </summary>
+        NYSE_LIFFE = 39,
+
+        /// <summary>
+        /// NYMEX Exempt Commercial Market LCH
+        /// </summary>
+        NYMEX_ECM = 40,
+
+        /// <summary>
+        /// NYMEX Multilateral trading Facility LCH
+        /// </summary>
+        NYMEX_MTF = 41,
+
+        /// <summary>
+        /// Chicago Board Of Trade Mini Sized Contracts
+        /// </summary>
+        CBOTMINI = 42,
+
+        /// <summary>
+        /// Chicago Mercantile Exchange Mini Sized Contracts
+        /// </summary>
+        CMEMINI = 43,
+
+        /// <summary>
+        /// US Futures Exchange
+        /// </summary>
+        USFE = 44,
+
+        /// <summary>
+        /// New York Mercantile Exchange Mini Sized Contracts
+        /// </summary>
+        NYMEXMINI = 45,
+
+        /// <summary>
+        /// The Green Exchange
+        /// </summary>
+        GREENX = 46,
+
+        /// <summary>
+        /// Toronto Stock Exchange
+        /// </summary>
+        TSE = 50,
+
+        /// <summary>
+        /// Montreal Stock Exchange
+        /// </summary>
+        MSE = 51,
+
+        /// <summary>
+        /// Canadian Venture Exchange
+        /// </summary>
+        CVE = 52,
+
+        /// <summary>
+        /// Winnipeg Stock Exchange
+        /// </summary>
+        WSE = 53,
+
+        /// <summary>
+        /// International Commodities Exchange Futures Canada
+        /// </summary>
+        ICEFC = 54,
+
+        /// <summary>
+        /// Bursa Malaysia Derivatives
+        /// </summary>
+        MDEX = 60,
+
+        /// <summary>
+        /// International Commodities Exchange Futures Derivatives
+        /// </summary>
+        ICEFI = 61,
+
+        /// <summary>
+        /// London Metals Exchange
+        /// </summary>
+        LME = 62,
+
+        /// <summary>
+        /// International Commodities Exchange Futures Europe
+        /// </summary>
+        ICEFE = 63,
+
+        /// <summary>
+        /// Dubai Mercantile Exchange
+        /// </summary>
+        DME = 65,
+
+        /// <summary>
+        /// Brazilian Mercantile & Future Exchange
+        /// </summary>
+        BMF = 66,
+
+        /// <summary>
+        /// Singapore International Monetary Exchange
+        /// </summary>
+        SGX = 67,
+
+        /// <summary>
+        /// European Exchange
+        /// </summary>
+        EUREX = 68,
+
+        /// <summary>
+        /// EuroNext Index Derivatives
+        /// </summary>
+        ENID = 69,
+
+        /// <summary>
+        /// EuroNext Interest Rates
+        /// </summary>
+        ENIR = 70,
+
+        /// <summary>
+        /// EuroNext Commodities
+        /// </summary>
+        ENCOM = 71,
+
+        /// <summary>
+        /// Tullett Liberty
+        /// </summary>
+        TULLETT = 72,
+
+        /// <summary>
+        /// Barclays Bank
+        /// </summary>
+        BARCLAYS = 73,
+
+        /// <summary>
+        /// Forex Capital Markets
+        /// </summary>
+        FXCM = 74,
+
+        /// <summary>
+        /// Warenterminborse Hannover
+        /// </summary>
+        WTB = 75,
+
+        /// <summary>
+        /// EUREX Indexes
+        /// </summary>
+        EUREXNDX = 76,
+
+        /// <summary>
+        /// EuroNext Indexes
+        /// </summary>
+        EURNXTNDX = 77,
+
+        /// <summary>
+        /// Tenfore Systems
+        /// </summary>
+        TENFORE = 78
+    }
+}
