2a3cc7949ce0b47c3c788395abb49786608bdc96
Mathieu Paquette <m.paquette@inovitex.com>
2018-06-02 13:06:21 -0400

    feat(symbol): added SecurityType

---
+++ src/IQFeed.CSharpApiClient/Lookup/Symbol/SecurityType.cs
@@ -0,0 +1,95 @@
+﻿namespace IQFeed.CSharpApiClient.Lookup.Symbol
+{
+    public enum SecurityType
+    {
+        /// <summary>
+        /// Equity
+        /// </summary>
+        EQUITY = 1,
+
+        /// <summary>
+        /// Index/Equity Option
+        /// </summary>
+        IEOPTION = 2,
+
+        /// <summary>
+        /// Mutual Fund
+        /// </summary>
+        MUTUAL = 3,
+
+        /// <summary>
+        /// Money Market Fund
+        /// </summary>
+        MONEY = 4,
+
+        /// <summary>
+        /// Bond
+        /// </summary>
+        BONDS = 5,
+
+        /// <summary>
+        /// Index
+        /// </summary>
+        INDEX = 6,
+
+        /// <summary>
+        /// Market Statistic
+        /// </summary>
+        MKTSTATS = 7,
+
+        /// <summary>
+        /// Future
+        /// </summary>
+        FUTURE = 8,
+
+        /// <summary>
+        /// Future Option
+        /// </summary>
+        FOPTION = 9,
+
+        /// <summary>
+        /// Future Spread
+        /// </summary>
+        SPREAD = 10,
+
+        /// <summary>
+        /// Spot (Cash) Price
+        /// </summary>
+        SPOT = 11,
+
+        /// <summary>
+        /// Forward
+        /// </summary>
+        FORWARD = 12,
+
+        /// <summary>
+        /// DTN Calculated Statistic
+        /// </summary>
+        CALC = 13,
+
+        /// <summary>
+        /// Calculated Strip
+        /// </summary>
+        STRIP = 14,
+
+        /// <summary>
+        /// Single Stock Future
+        /// </summary>
+        SSFUTURE = 15,
+
+        /// <summary>
+        /// Foreign Monetary Exchange
+        /// </summary>
+        FOREX = 16,
+
+        /// <summary>
+        /// Future Market Depth
+        /// </summary>
+        MKTDEPTH = 17,
+
+        /// <summary>
+        /// Precious Metals
+        /// </summary>
+        PRECMTL = 18,
+    }
+}
