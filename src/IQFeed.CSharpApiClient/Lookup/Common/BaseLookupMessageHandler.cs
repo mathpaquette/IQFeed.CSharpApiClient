48cb0d268158d73a6a2ebed8843ea2db288b6d83
Vardan Tovmasyan <mrut2pac@msn.com>
2019-05-21 21:47:16 -0700

    bug fixes

--- src/IQFeed.CSharpApiClient/Lookup/Common/BaseLookupMessageHandler.cs
+++ src/IQFeed.CSharpApiClient/Lookup/Common/BaseLookupMessageHandler.cs
@@ -10,8 +10,9 @@ namespace IQFeed.CSharpApiClient.Lookup.Common
     public abstract class BaseLookupMessageHandler
     {
         private static readonly string EndMessagePattern = IQFeedDefault.ProtocolEndOfMessageCharacters + IQFeedDefault.ProtocolDelimiterCharacter;
+        private static readonly string NoDataIndicator = "E,!NO_DATA!";
         private static readonly Regex ErrorPattern = new Regex(@"^E,.*,$");
-        private static readonly Regex ErrorPatternWithRequestId = new Regex(@"^.*,E,.*,$");
+        private static readonly Regex ErrorPatternWithRequestId = new Regex(@"^.*,E,.*,$");        
 
         protected static MessageContainer<T> ProcessMessages<T>(Func<string, T> parser, byte[] message, int count, bool hasRequestId)
         {
@@ -21,11 +22,20 @@ namespace IQFeed.CSharpApiClient.Lookup.Common
             var endMsg = false;
             var lastMsgIdx = messages.Length - 1;
 
+            if (messages.Length == 1)
+            {
+                if(IsErrorMessage(messages[0], hasRequestId))
+                    return new MessageContainer<T>(parsedMessages, true, messages[0]);
+            }
+
             for (var i = 0; i < messages.Length; i++)
             {
+                if(IsNoDataMessage(messages[0], hasRequestId))
+                    return new MessageContainer<T>(parsedMessages, true);
+
                 // check for error
                 if(IsErrorMessage(messages[i], hasRequestId))
-                    return new MessageContainer<T>(parsedMessages, true, messages[i]);                    
+                    return new MessageContainer<T>(parsedMessages, true, messages[i]);                
 
                 // check for end (only last message)
                 if (i == lastMsgIdx && messages[i].EndsWith(EndMessagePattern))
@@ -59,6 +69,26 @@ namespace IQFeed.CSharpApiClient.Lookup.Common
                     && message[0] == IQFeedDefault.PrototolErrorCharacter // first one is the error marker
                     && message[1] == IQFeedDefault.ProtocolDelimiterCharacter // the next one is a delimiter
                     && ErrorPatternWithRequestId.IsMatch(message); // matches the error pattern
+            }
+        }
+
+        private static bool IsNoDataMessage(string message, bool hasRequestId)
+        {
+            if(hasRequestId)
+            {
+                int firstDelimiterIndex = message.IndexOf(IQFeedDefault.ProtocolDelimiterCharacter);
+                return firstDelimiterIndex != -1 // there is a delimiter
+                    && firstDelimiterIndex + 2 < message.Length // there are 2 more characters after that
+                    && message[firstDelimiterIndex + 1] == IQFeedDefault.PrototolErrorCharacter // next is the error marker
+                    && message[firstDelimiterIndex + 2] == IQFeedDefault.ProtocolDelimiterCharacter // and delimiter after that
+                    && message.Contains(NoDataIndicator);
+            }
+            else
+            {
+                return message.Length > 2 // there are 2 or more characters
+                    && message[0] == IQFeedDefault.PrototolErrorCharacter // first one is the error marker
+                    && message[1] == IQFeedDefault.ProtocolDelimiterCharacter // the next one is a delimiter
+                    && message.StartsWith(NoDataIndicator);
             }
         }
     }
