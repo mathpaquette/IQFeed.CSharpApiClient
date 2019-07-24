99171176ee06cd3e2671ec9a5d898a389b350c0c
Mathieu Paquette <m.paquette@inovitex.com>
2019-07-24 11:22:55 -0400

    fix(streaming): fix bad ReqBarWatch request format in DerivativeRequestFormatter
    fixes #41

--- src/IQFeed.CSharpApiClient.Tests/Streaming/Derivative/DerivativeRequestFormatterTests.cs
+++ src/IQFeed.CSharpApiClient.Tests/Streaming/Derivative/DerivativeRequestFormatterTests.cs
@@ -18,7 +18,7 @@ namespace IQFeed.CSharpApiClient.Tests.Streaming.Derivative
         public void Should_Format_BarWatch()
         {
             var request = _derivativeRequestFormatter.ReqBarWatch("aapl", 60, new DateTime(2018, 01, 01, 09, 30, 00), 10, 1000, new TimeSpan(9, 30, 00), new TimeSpan(16, 00, 00), "TEST", DerivativeIntervalType.S, 5);
-            Assert.AreEqual(request, "BW,AAPL,60,20180101 093000,10,1000,093000,160000,TEST,s,5,\r\n");
+            Assert.AreEqual(request, "BW,AAPL,60,20180101 093000,10,1000,093000,160000,TEST,s,,5\r\n");
         }
 
         [Test]
