ec857209b67a72fab228f97d3f2addaa8964a923
Mathieu Paquette <me@mathpaquette.com>
2020-05-16 13:49:51 -0400

    feat(extensions): resample TickMessage by tick count

--- src/IQFeed.CSharpApiClient.Extensions/Lookup/Historical/Resample/TickMessageExtensions.cs
+++ src/IQFeed.CSharpApiClient.Extensions/Lookup/Historical/Resample/TickMessageExtensions.cs
@@ -19,6 +19,16 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
                 : ToHistoricalBarsAscending(tickMessages, interval);
         }
 
+        public static IEnumerable<HistoricalBar<double>> ToHistoricalBars(
+            this IEnumerable<ITickMessage<double>> tickMessages,
+            int ticks,
+            DataDirection direction = DataDirection.Newest)
+        {
+            return direction == DataDirection.Newest
+                ? ToHistoricalBarsDescending(tickMessages, ticks)
+                : ToHistoricalBarsAscending(tickMessages, ticks);
+        }
+
         public static IEnumerable<HistoricalBar<double>> ToHistoricalBarsUncompressed(
             this IEnumerable<ITickMessage<double>> tickMessages,
             TimeSpan interval,
@@ -111,6 +121,84 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
         private static IEnumerable<HistoricalBar<double>> ToHistoricalBarsDescending(this IEnumerable<ITickMessage<double>> tickMessages, TimeSpan interval)
         {
             return tickMessages.Reverse().ToHistoricalBarsAscending(interval).Reverse();
+        }
+
+        private static IEnumerable<HistoricalBar<double>> ToHistoricalBarsAscending(this IEnumerable<ITickMessage<double>> tickMessages, int ticks)
+        {
+            HistoricalBar<double> currentBar = null;
+
+            var totalVolume = 0;
+            var totalTrade = 0;
+
+            foreach (var tick in tickMessages)
+            {
+                // skip if trade size is zero
+                if (tick.LastSize == 0)
+                    continue;
+
+                totalVolume += tick.LastSize;
+                totalTrade += 1;
+
+                // to effect the price the trade must be C or E
+                if (tick.BasisForLast == 'O')
+                    continue;
+
+                if (currentBar != null)
+                {
+                    // reset the counts if dates differ
+                    if (tick.Timestamp.Date != currentBar.Timestamp.Date)
+                    {
+                        totalVolume = tick.LastSize;
+                        totalTrade = 1;
+                    }
+                    else if (currentBar.PeriodTrade < ticks)
+                    {
+                        if (tick.Last < currentBar.Low)
+                            currentBar.Low = tick.Last;
+
+                        if (tick.Last > currentBar.High)
+                            currentBar.High = tick.Last;
+
+                        currentBar.Close = tick.Last;
+
+                        currentBar.PeriodVolume += tick.LastSize;
+                        currentBar.PeriodTrade += 1;
+
+                        currentBar.TotalVolume = totalVolume;
+                        currentBar.TotalTrade = totalTrade;
+
+                        currentBar.Wap += tick.Last * tick.LastSize;
+                        continue;
+                    }
+
+                    currentBar.Wap = currentBar.Wap / currentBar.PeriodVolume;
+                    yield return currentBar;
+                }
+
+                currentBar = new HistoricalBar<double>(
+                    tick.Timestamp,
+                    tick.Last,
+                    tick.Last,
+                    tick.Last,
+                    tick.Last,
+                    tick.LastSize,
+                    1,
+                    totalVolume,
+                    totalTrade,
+                    tick.Last * tick.LastSize);
+            }
+
+            // return the last created bar when last tick reached
+            if (currentBar != null)
+            {
+                currentBar.Wap = currentBar.Wap / currentBar.PeriodVolume;
+                yield return currentBar;
+            }
+        }
+
+        private static IEnumerable<HistoricalBar<double>> ToHistoricalBarsDescending(this IEnumerable<ITickMessage<double>> tickMessages, int ticks)
+        {
+            return tickMessages.Reverse().ToHistoricalBarsAscending(ticks).Reverse();
         }
 
         private static IEnumerable<HistoricalBar<double>> ToHistoricalBarsUncompressedAscending(this IEnumerable<ITickMessage<double>> tickMessages, TimeSpan interval)
