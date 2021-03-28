7b08b52bee7891230f013f111b47344602cb3808
mrut2pac <mrut2pac@users.noreply.github.com>
2021-03-27 20:19:07 -0700

    Revert "Added logic to handle invalid symbol snapshot queries to fail fast and with a proper exception rather than a timeout."
    This reverts commit 75f1fa7634408648ee7cd449c02a4b893dd85d73.

--- src/IQFeed.CSharpApiClient/Common/Exceptions/SymbolNotFoundIQFeedException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IQFeed.CSharpApiClient.Common.Exceptions
-{
-    // ReSharper disable once InconsistentNaming
-    public class SymbolNotFoundIQFeedException : IQFeedException
-    {
-        public SymbolNotFoundIQFeedException(string symbol) : base(string.Empty, $"The specified Symbol '{symbol}' wasn't found on IQFeed.", string.Empty, string.Empty) { }
-    }
-}
