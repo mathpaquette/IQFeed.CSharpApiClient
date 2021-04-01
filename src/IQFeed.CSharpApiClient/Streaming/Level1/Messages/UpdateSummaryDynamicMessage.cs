a6a56abd0767e338884df6f47fe303a9db00b536
mrut2pac <mrut2pac@users.noreply.github.com>
2021-03-31 17:35:54 -0700

    1. Moved the field parsing methods into a helper class and updated some of the references 2. Added all "dynamic" field properties to IUpdateSummaryMessage, marking the DynamicFields property as a candidate for removal 3. Added NotImplementedException throwing stubs for the added "dynamic" properties for UpdateSummaryMessage and UpdateSummaryDynamicMessage classes 4. Added new static class that allows generating UpdateSummaryMessage implementations classes on the fly for the selected dynamic fields. Used advanced IL code generation from Reflection.Emit. Tried my best to add enough comments to make the code reading less of a nightmare. :) 5. Added unit tests to cover all of the functionality for the generated objects 6. Added a new Level1MessageUniversalHandler which utilizes the runtime generated classes and allows changing the dynamic fields without need to recreate the client 7. Added an example how to use Level1MessageUniversalHandler 8. Added references to System.Reflection.Emit and System.Reflection.Emit.ILGeneration packages in the client project 9. Added few methods to StringExtensions to shortcut the IL code generation

--- src/IQFeed.CSharpApiClient/Streaming/Level1/Messages/UpdateSummaryDynamicMessage.cs
+++ src/IQFeed.CSharpApiClient/Streaming/Level1/Messages/UpdateSummaryDynamicMessage.cs
@@ -22,6 +22,66 @@ namespace IQFeed.CSharpApiClient.Streaming.Level1.Messages
         public string MostRecentTradeConditions => throw new Exception(GetErrorMessage(nameof(MostRecentTradeConditions)));
         public Level1DynamicFields DynamicFields { get; private set; }
 
+        #region Dynamic Fields
+
+        private readonly string DynamicFieldNotAvailableErrorMessage = $"Dynamic Field is not available. Please use {nameof(ILevel1Client)}.{nameof(ILevel1Client.SelectUpdateFieldNamesV2)} to enable!";
+
+        public double SevenDayYield => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double AskChange => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int AskMarketCenter => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public TimeSpan AskTime => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string AvailableRegions => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double AverageMaturity => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double BidChange => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int BidMarketCenter => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public TimeSpan BidTime => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Change => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double ChangeFromOpen => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double CloseRange1 => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double CloseRange2 => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string DaysToExpiration => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string DecimalPrecision => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int Delay => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string ExchangeID => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double ExtendedTrade => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public DateTime ExtendedTradeDate => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int ExtendedTradeMarketCenter => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int ExtendedTradeSize => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public TimeSpan ExtendedTradeTime => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double ExtendedTradingChange => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double ExtendedTradingDifference => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string FinancialStatusIndicator => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string FractionDisplayCode => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Last => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public DateTime LastDate => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int LastMarketCenter => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int LastSize => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public TimeSpan LastTime => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double MarketCapitalization => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int MarketOpen => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public DateTime MostRecentTradeDate => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double NetAssetValue => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int NumberOfTradesToday => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int OpenInterest => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double OpenRange1 => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double OpenRange2 => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double PercentChange => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double PercentOffAverageVolume => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int PreviousDayVolume => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double PriceEarningsRatio => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Range => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string RestrictedCode => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Settle => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public DateTime SettlementDate => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Spread => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int Tick => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public int TickID => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public string Type => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double Volatility => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+        public double VWAP => throw new NotImplementedException(DynamicFieldNotAvailableErrorMessage);
+
+        #endregion Dynamic Fields
+
         public UpdateSummaryDynamicMessage(Level1DynamicFields dynamicFields)
         {
             DynamicFields = dynamicFields;
