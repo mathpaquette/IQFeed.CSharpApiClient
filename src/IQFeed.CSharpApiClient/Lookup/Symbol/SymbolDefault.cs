89895c6cf88b416863cc8b2a4cd0d9af8c57fdb1
Nich Overend <nich@opusinvestors.com>
2021-04-05 17:38:20 +0100

    Lookup.Symbol protocol 6.2

--- src/IQFeed.CSharpApiClient/Lookup/Symbol/SymbolDefault.cs
+++ src/IQFeed.CSharpApiClient/Lookup/Symbol/SymbolDefault.cs
@@ -3,6 +3,14 @@
     public class SymbolDefault
     {
         public const string MarketSymbolsArchiveUrl = "http://www.dtniq.com/product/mktsymbols_v2.zip";
+ 
         public const string ExpiredOptionsArchiveUrl = "ftp://www.dtniq.com/beta/IEOPTION.zip";
+        
+        /// <summary>
+        /// We can't check the length of values on most of the messages in this group,
+        /// as some description fields can contain commas (giving us a variable number of
+        /// fields, so we need to look for this DataId in the correct field instead.
+        /// </summary>
+        public const string SymbolsDataId = "LS";
     }
 }
