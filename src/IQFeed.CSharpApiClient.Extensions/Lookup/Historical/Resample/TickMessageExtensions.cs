f5aa66c1741010816ca7ccb73c53207adc11f68f
Mathieu Paquette <me@mathpaquette.com>
2021-07-04 09:21:25 -0400

    fix(extensions): VWAP duplicated size

--- src/IQFeed.CSharpApiClient.Extensions/Lookup/Historical/Resample/TickMessageExtensions.cs
+++ src/IQFeed.CSharpApiClient.Extensions/Lookup/Historical/Resample/TickMessageExtensions.cs
@@ -19,16 +19,6 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
                 : ToHistoricalBarsAscending(tickMessages, interval);
         }
 
-        public static IEnumerable<HistoricalBar> ToHistoricalBars(
-            this IEnumerable<ITickMessage> tickMessages,
-            int ticks,
-            DataDirection direction = DataDirection.Newest)
-        {
-            return direction == DataDirection.Newest
-                ? ToHistoricalBarsDescending(tickMessages, ticks)
-                : ToHistoricalBarsAscending(tickMessages, ticks);
-        }
-
         public static IEnumerable<HistoricalBar> ToHistoricalBarsUncompressed(
             this IEnumerable<ITickMessage> tickMessages,
             TimeSpan interval,
@@ -59,7 +49,7 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
                 if (currentBar != null && tick.Timestamp >= nextTimestamp)
                 {
                     // We've reached the end of this bar. Send it out! Setting the currentBar to null
-                    //   will cause a new bar to be generated on the first valid tick, below.
+                    // will cause a new bar to be generated on the first valid tick, below.
                     currentBar.VWAP = currentBar.VWAP / currentBar.PeriodVolume;
                     yield return currentBar;
 
@@ -67,12 +57,12 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
                 }
 
                 // Check for date change up here otherwise 'O' ticks will be ignored
-                //   if they are the first ticks of the day, which will make total volume incorrect
+                // if they are the first ticks of the day, which will make total volume incorrect
                 if (currentDate != null && tick.Timestamp.Date != currentDate)
                 {
                     // Date has changed. Reset the numbers for the next one
                     // Don't start a new bar here, because if the next tick is an 'O' we want to count the volume
-                    //  but not the tick, and if it's the only tick in a bar, then there shouldn't be a bar! (Apparently)
+                    // but not the tick, and if it's the only tick in a bar, then there shouldn't be a bar! (Apparently)
                     totalVolume = 0;
                     totalTrade = 0;
                     currentDate = tick.Timestamp.Date;
@@ -99,7 +89,7 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
                         0,
                         0,
                         0,
-                        tick.Last * tick.LastSize);
+                        0);
                     nextTimestamp = currentTimestamp.AddTicks(intervalTicks);
                     currentDate = currentTimestamp.Date;
                 }
@@ -132,84 +122,6 @@ namespace IQFeed.CSharpApiClient.Extensions.Lookup.Historical.Resample
         private static IEnumerable<HistoricalBar> ToHistoricalBarsDescending(this IEnumerable<ITickMessage> tickMessages, TimeSpan interval)
         {
             return tickMessages.Reverse().ToHistoricalBarsAscending(interval).Reverse();
-        }
-
-        private static IEnumerable<HistoricalBar> ToHistoricalBarsAscending(this IEnumerable<ITickMessage> tickMessages, int ticks)
-        {
-            HistoricalBar currentBar = null;
-
-            var totalVolume = 0;
-            var totalTrade = 0;
-
-            foreach (var tick in tickMessages)
-            {
-                // skip if trade size is zero
-                if (tick.LastSize == 0)
-                    continue;
-
-                totalVolume += tick.LastSize;
-                totalTrade += 1;
-
-                // to effect the price the trade must be C or E
-                if (tick.BasisForLast == 'O')
-                    continue;
-
-                if (currentBar != null)
-                {
-                    // reset the counts if dates differ
-                    if (tick.Timestamp.Date != currentBar.Timestamp.Date)
-                    {
-                        totalVolume = tick.LastSize;
-                        totalTrade = 1;
-                    }
-                    else if (currentBar.PeriodTrade < ticks)
-                    {
-                        if (tick.Last < currentBar.Low)
-                            currentBar.Low = tick.Last;
-
-                        if (tick.Last > currentBar.High)
-                            currentBar.High = tick.Last;
-
-                        currentBar.Close = tick.Last;
-
-                        currentBar.PeriodVolume += tick.LastSize;
-                        currentBar.PeriodTrade += 1;
-
-                        currentBar.TotalVolume = totalVolume;
-                        currentBar.TotalTrade = totalTrade;
-
-                        currentBar.VWAP += tick.Last * tick.LastSize;
-                        continue;
-                    }
-
-                    currentBar.VWAP = currentBar.VWAP / currentBar.PeriodVolume;
-                    yield return currentBar;
-                }
-
-                currentBar = new HistoricalBar(
-                    tick.Timestamp,
-                    tick.Last,
-                    tick.Last,
-                    tick.Last,
-                    tick.Last,
-                    totalVolume,
-                    tick.LastSize,
-                    totalTrade,
-                    1,
-                    tick.Last * tick.LastSize);
-            }
-
-            // return the last created bar when last tick reached
-            if (currentBar != null)
-            {
-                currentBar.VWAP = currentBar.VWAP / currentBar.PeriodVolume;
-                yield return currentBar;
-            }
-        }
-
-        private static IEnumerable<HistoricalBar> ToHistoricalBarsDescending(this IEnumerable<ITickMessage> tickMessages, int ticks)
-        {
-            return tickMessages.Reverse().ToHistoricalBarsAscending(ticks).Reverse();
         }
 
         private static IEnumerable<HistoricalBar> ToHistoricalBarsUncompressedAscending(this IEnumerable<ITickMessage> tickMessages, TimeSpan interval)
