47799e028ea244240452c2d4d3552763d5e38eb9
Rob Hague <5132141+rob-hague@users.noreply.github.com>
2021-10-28 12:15:01 +0100

    fix(tests): fix typo in MarketSummary FDS request test

--- src/IQFeed.CSharpApiClient.Tests.Integration/Lookup/MarketSummary/MarketSummaryFacadeTests.cs
+++ src/IQFeed.CSharpApiClient.Tests.Integration/Lookup/MarketSummary/MarketSummaryFacadeTests.cs
@@ -48,7 +48,7 @@ namespace IQFeed.CSharpApiClient.Tests.Integration.Lookup.MarketSummary
         [Test]
         public async Task Should_Return_MarketSummaryMessages_When_GetFundamentalSummaryAsync()
         {
-            var marketSummaryMessages = await _lookupClient.MarketSummary.GetEndOfDaySummaryAsync(Security_Type, GroupId, new DateTime(2020, 04, 06));
+            var marketSummaryMessages = await _lookupClient.MarketSummary.GetEndOfDayFundamentalSummaryAsync(Security_Type, GroupId, new DateTime(2020, 04, 06));
             Assert.Greater(marketSummaryMessages.Count(), 0);
         }
 
