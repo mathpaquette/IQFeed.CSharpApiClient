e23011de85ff96518c1959765f95a90c490cccb1
mathpaquette <me@mathpaquette.com>
2022-11-11 07:31:56 -0500

    feat(lookup): update expired options URL

--- src/IQFeed.CSharpApiClient/Lookup/Symbol/ExpiredOptions/ExpiredOption.cs
+++ src/IQFeed.CSharpApiClient/Lookup/Symbol/ExpiredOptions/ExpiredOption.cs
@@ -10,30 +10,31 @@ namespace IQFeed.CSharpApiClient.Lookup.Symbol.ExpiredOptions
 
         public EquityOption EquityOption { get; }
         public int SecurityType { get; }
-        public string Title { get; }
+        public string Description { get; }
         public int Exchange { get; }
         public DateTime LastUpdate { get; }
 
         public static ExpiredOption Parse(string line)
         {
-            // ZZQ1020B30,2, FEB 10 C 30,,14,20100114
+            // Symbol,SecurityType,Description,ExpirationDate,Exchange,LastUpdate
+            // CIEN1808R23.5,2,CIEN JUN 2018 P 23.50,20180608,14,20180612
             var values = line.Split(',');
 
             var equityOption = EquityOption.Parse(values[0]);
             int.TryParse(values[1], out var securityType);
             var title = values[2].Trim();
-            // skip values[3]
+            // skip values[3] ExpirationDate already part of EquityOption
             int.TryParse(values[4], out var exchange);
             DateTime.TryParseExact(values[5], LastUpdateDateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var lastUpdate);
 
             return new ExpiredOption(equityOption, securityType, title, exchange, lastUpdate);
         }
 
-        public ExpiredOption(EquityOption equityOption, int securityType, string title, int exchange, DateTime lastUpdate)
+        public ExpiredOption(EquityOption equityOption, int securityType, string description, int exchange, DateTime lastUpdate)
         {
             EquityOption = equityOption;
             SecurityType = securityType;
-            Title = title;
+            Description = description;
             Exchange = exchange;
             LastUpdate = lastUpdate;
         }
