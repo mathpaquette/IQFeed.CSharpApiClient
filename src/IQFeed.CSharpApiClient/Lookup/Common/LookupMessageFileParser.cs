0c38e8fbb6c61aa988930de904526182f33aaff1
mathpaquette <me@mathpaquette.com>
2023-05-25 10:42:51 -0400

    feat(lookup): add ability to parse compressed archive

--- src/IQFeed.CSharpApiClient/Lookup/Common/LookupMessageFileParser.cs
+++ src/IQFeed.CSharpApiClient/Lookup/Common/LookupMessageFileParser.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 
 namespace IQFeed.CSharpApiClient.Lookup.Common
 {
     public class LookupMessageFileParser
     {
-        public static IEnumerable<T> ParseFromFile<T>(Func<string, T> parserFunc, string path)
+        public static IEnumerable<T> ParseFromFile<T>(Func<string, T> parseFunc, string path)
         {
-            using (var file = new StreamReader(path))
+            return ParseFromFile(parseFunc, new StreamReader(path));
+        }
+
+        public static IEnumerable<T> ParseFromFile<T>(Func<string, T> parseFunc, Stream stream)
+        {
+            return ParseFromFile(parseFunc, new StreamReader(stream));
+        }
+
+        public static IEnumerable<T> ParseFromFile<T>(Func<string, T> parseFunc, StreamReader file)
+        {
+            using (file)
             {
                 string line;
                 while ((line = file.ReadLine()) != null)
@@ -16,7 +27,24 @@ namespace IQFeed.CSharpApiClient.Lookup.Common
                     if (string.IsNullOrEmpty(line) || line[0] == '!')
                         continue;
 
-                    yield return parserFunc(line);
+                    yield return parseFunc(line);
+                }
+            }
+        }
+
+        public static IEnumerable<T> ParseFromArchive<T>(Func<string, T> parseFunc, string zipPath)
+        {
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    using (var stream = entry.Open())
+                    {
+                        foreach (var message in ParseFromFile(parseFunc, stream))
+                        {
+                            yield return message;
+                        }
+                    }
                 }
             }
         }
